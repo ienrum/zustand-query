2dfa5adb791774ad101be9fc3ab9e9095f08db35
ienrum <azang0506@gmail.com>
2024-04-16 12:37:04 +0900

    test: Add unit tests for the useMutate hook

---
+++ tests/mutate/store.test.ts
@@ -0,0 +1,153 @@
+import { renderHook } from "@testing-library/react-hooks"
+import { server } from "../../mock/node"
+import {
+  afterEach,
+  describe,
+  expect,
+  beforeAll,
+  afterAll,
+  it,
+  vi,
+} from "vitest"
+import axios, { AxiosError } from "axios"
+import useMutate from "../../src/mutate/useMutate"
+
+// Define test data, (this could be an API response)
+interface TestDatas {
+  id: number
+  name: string
+}
+
+export const testDatas: TestDatas[] = [
+  { id: 1, name: "Test User 1" },
+  { id: 2, name: "Test User 2" },
+]
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe("mutateData", () => {
+  it("should fetch data successfully", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () =>
+          await axios.post<TestDatas>("/user", {
+            name: "John Doe",
+          }),
+      })
+    )
+
+    await waitFor(() => result.current.isSettled)
+
+    expect(result.current.data?.data).toMatchObject(testDatas[0])
+    expect(result.current.error).toBeNull()
+    expect(result.current.status).toBe("success")
+  })
+
+  it("should handle a server error", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () => await axios.get<TestDatas>("/user/3"),
+      })
+    )
+    await waitFor(() => result.current.isSettled)
+
+    expect(result.current.error).not.toBeNull()
+    expect(result.current.status).toBe("error")
+  })
+
+  it("should handle a network error", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () => await axios.get<TestDatas>("/error"),
+      })
+    )
+    await waitFor(() => result.current.isSettled)
+
+    expect(result.current.error).toBeDefined()
+    expect(result.current.status).toBe("error")
+  })
+})
+
+describe("config store", () => {
+  it("should be success by fetch api", async () => {
+    const fetchUser1 = async () => await fetch("/user/1")
+
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: fetchUser1,
+      })
+    )
+    await waitFor(() => result.current.isSettled)
+
+    expect(await result.current.data?.json()).toMatchObject(testDatas[0])
+    expect(result.current.status).toBe("success")
+    expect(result.current.error).toBeNull()
+  })
+
+  it("should be success by axios api", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () => await axios.get<TestDatas>("/user/2"),
+      })
+    )
+
+    await waitFor(() => result.current.isSettled)
+
+    expect(result.current.data?.data).toMatchObject(testDatas[1])
+    expect(result.current.status).toBe("success")
+    expect(result.current.error).toBeNull()
+  })
+
+  it("should be error by fetch api", async () => {
+    const fetchUser3 = async () => await fetch("/user/3")
+
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: fetchUser3,
+      })
+    )
+
+    await waitFor(() => result.current.isSettled)
+
+    expect(result.current.data?.ok).toBe(false)
+  })
+})
+
+describe("onSuccess and onError", () => {
+  it("should call onSuccess", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () => await axios.get<TestDatas>("/user/1"),
+        onSuccess: (data) => console.info(data),
+      })
+    )
+
+    const consoleSpy = vi.spyOn(console, "info")
+
+    await waitFor(() => result.current.isSettled)
+
+    expect(consoleSpy).toHaveBeenCalled()
+    expect(result.current.data?.data).toMatchObject(testDatas[0])
+
+    consoleSpy.mockRestore()
+  })
+
+  it("should call onError", async () => {
+    const { result, waitFor } = renderHook(() =>
+      useMutate({
+        onMutate: async () => await axios.get<TestDatas>("/user/3"),
+        onError: (error: AxiosError) => console.info(error),
+      })
+    )
+    const consoleSpy = vi.spyOn(console, "info")
+
+    await waitFor(() => result.current.isSettled)
+
+    expect(consoleSpy).toHaveBeenCalled()
+    expect(result.current.error).toBeDefined()
+
+    consoleSpy.mockRestore()
+  })
+})
