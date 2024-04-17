28db826872ae9a6b7ddebcd496e0580ce1bf1d02
ienrum <azang0506@gmail.com>
2024-04-17 20:45:39 +0900

    feat: Add useFetch hook to use mutateFn

--- src/mutate/useMutate.ts
+++ src/mutate/useMutate.ts
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react"
 import { createUseMutateStore } from "./mutateStore"
 
-interface UseFetchParams<TResponse, TError> {
-  onMutate: () => Promise<TResponse>
+interface UseFetchParams<TData, TResponse, TError> {
+  onMutate?: () => Promise<TResponse>
   onError?: (error: TError) => void
   onSuccess?: (data: TResponse) => void
+  mutateFn?: (data: TData) => Promise<TResponse>
 }
 
-const useFetch = <TResponse, TError = Error>({
+const useFetch = <TData = void, TResponse = any, TError = Error>({
   onMutate,
   onError,
   onSuccess,
-}: UseFetchParams<TResponse, TError>) => {
+  mutateFn,
+}: UseFetchParams<TData, TResponse, TError>) => {
   const [useStore, _] = useState(() =>
     createUseMutateStore<TResponse, TError>()
   )
@@ -24,10 +26,22 @@ const useFetch = <TResponse, TError = Error>({
   }, [store.status])
 
   useEffect(() => {
-    store.mutateData(onMutate)
+    onMutate && store.mutateData(onMutate)
   }, [])
 
-  return store
+  const mutate = (
+    data: TData,
+    onError?: (error: TError) => void,
+    onSuccess?: (data: TResponse) => void
+  ) => {
+    if (mutateFn) {
+      store.mutateData(() => mutateFn(data))
+      onError && store.error && onError(store.error)
+      onSuccess && store.data && onSuccess(store.data)
+    }
+  }
+
+  return { ...store, mutate }
 }
 
 export default useFetch
